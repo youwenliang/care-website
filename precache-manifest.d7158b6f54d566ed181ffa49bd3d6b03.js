17c054af8719102f9bb2ce5a0fa5c948db528526
youwenliang <youwen.mark.liang@gmail.com>
2021-04-02 17:24:10 +0800

    Updates

---
+++ precache-manifest.d7158b6f54d566ed181ffa49bd3d6b03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9540ef2f1cb58ca554d8b2d4ac7b6113",
+    "url": "/care-website/index.html"
+  },
+  {
+    "revision": "2869cee1c98a6ebe3921",
+    "url": "/care-website/static/css/2.40c68a69.chunk.css"
+  },
+  {
+    "revision": "ee76c03eff52ae9d6820",
+    "url": "/care-website/static/css/main.afb50390.chunk.css"
+  },
+  {
+    "revision": "2869cee1c98a6ebe3921",
+    "url": "/care-website/static/js/2.398e304f.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/care-website/static/js/2.398e304f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee76c03eff52ae9d6820",
+    "url": "/care-website/static/js/main.a5a49254.chunk.js"
+  },
+  {
+    "revision": "fdd5fe4b6deddb6ee06a",
+    "url": "/care-website/static/js/runtime-main.6da982ed.js"
+  }
+]);
